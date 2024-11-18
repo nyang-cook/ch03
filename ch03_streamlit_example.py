2ff53da1df1c87383e265e2b331ad8357d5cc8d3
nyang <ysy825@gachon.ac.kr>
2024-11-18 12:40:45 +0900

    first commit

---
+++ ch03_streamlit_example.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("나의 첫 번째 streamlit 앱")
+
+st.header("streamlit에 오신 것을 환영합니다.")
+st.subheader("웹 앱을 만들기 위한 강력하고 ")
+
+st.text("이것은 일반 텍스트입니다")
+
+st.write("write() 함수를 사용하여 텍스트, 데이터 또는 플롯을 표시할 수도 있습니다. ")
---
+++ ch03_summarize_text.py
@@ -0,0 +1,40 @@
+#### 기본 정보 입력 ####
+# 스트림릿 패키지  설치
+import streamlit as st
+# OpenAI 패키지 추가
+import openai
+
+#### 기능 구현 함수 정리####
+def askGpt(prompt, apikey):
+    client = openai.OpenAI(api_key = apikey)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        message=[{"role":"user", "content":prompt}]
+    )
+    gptResponse = response.choices[0].message.content
+    return gptResponse
+
+#### 메인 함수 ####
+def main():
+    st.set_page_config(page_title="요약 프로그램")
+    # sesstion state 초기화
+    if "OPENAI_API" not in st.seesion_state:
+        st.session_state["OPENAI_API"] = ""
+
+    # 사이드바
+    with st.sidebar:
+        # Open AI API 키 입력받기
+        openai_apikey = st.text_input(label="OPENAI API 키", placeholder="Enter your API Key", value='', type='password')
+        # 세션 스테이트에 저장
+        if openai_apikey:
+            st.session_state["OPENAI_API"] = openai_apikey
+        st.markdown('---')
+        # 메인 공간
+        st.header("문서 요약 프로그램")
+        st.markdown("---")
+
+        text = st.text_area("요약 할 글을 입력하세요")
+        if st.button("요약"):
+            prompt = f'''
+            **instruction**  :
+            - 
